a15b9e44bfedc6907f10e1cff8fe5e9411929776
Chris Jobling <c.p.jobling@swansea.ac.uk>
2013-04-23 19:22:45 +0100

    New rails app

---
+++ README.md
@@ -0,0 +1,12 @@
+# Todos on Rails
+
+## 1. *Create a new rails project*
+
+    $ rails new todos_rails
+
+## 2. *Add to Git*
+
+    $ cd todos_rails
+    $ git init .
+    $ git add .
+    $ git commit -m "New rails app"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodosRails::Application.config.secret_token = '090b6451ad44b5e403ecea51c3b2ac9a58c6b898602567433aaa103120b26b7c66411c258479bb1a064cb378cc3d07f6e1e2aa394cab3371f71f3a8fe1fd4991'
